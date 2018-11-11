4a61cf04dc67b3f99e45739cd42c46be164889a0
homelandsshi <homelandsshi@users.noreply.github.com>
2018-11-11 11:14:43 +0000

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,10 @@
+import bisect
 import random
 
 # a random integer N: a <= N <= b
 N = random.randint(a, b) 
+
+# binary search
+# find index to insert x into a 
+i = bisect.bisect(a, x, low=0, high=len(a))      # after x if x exists
+i = bisect.bisect_left(a, x, low=0, high=len(a)) # before x if x exists
