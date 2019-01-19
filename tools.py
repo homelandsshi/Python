a837fef0dfd30cb6f73cce5f0cc08da6cb339b52
homelandsshi <homelandsshi@users.noreply.github.com>
2019-01-19 09:52:45 +0000

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 import bisect
+impot copy
 import random
 
 # a random integer N: a <= N <= b
@@ -8,3 +9,7 @@ N = random.randint(a, b)
 # find index to insert x into a 
 i = bisect.bisect(a, x, low=0, high=len(a))      # after x if x exists
 i = bisect.bisect_left(a, x, low=0, high=len(a)) # before x if x exists
+
+# deep copy
+l = [1]
+l2 = copy.deepcopy(l)
